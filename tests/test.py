81b016da707a4c9584093f797bdcc449369e2bf1
Praween Chhetri <62017020+Pra-wnn@users.noreply.github.com>
2024-09-30 16:32:18 +0545

    Success Scenario

--- tests/test.py
+++ tests/test.py
@@ -16,7 +16,7 @@ class AppTestCase(unittest.TestCase):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
         # Check if the title of the page is in the response data
-        self.assertIn(b'Flask MariaDB Examplessd', response.data)
+        self.assertIn(b'Flask MariaDB Example', response.data)
     
     
 
